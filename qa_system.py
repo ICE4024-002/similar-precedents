96cc839c7e05b49880a06ecac754dbf4b488c68b
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-04-23 14:43:37 +0900

    similar_precedent import 코드 오타 수정

--- qa_system.py
+++ qa_system.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-import similar_precendent
+import similar_precedent
 
 client = OpenAI(
     # This is the default and can be omitted
@@ -7,7 +7,7 @@ client = OpenAI(
 )
 
 userInput = "저는 甲대학병원에서 전공의 과정을 밟고 있는 레지던트입니다. 그런데 몇 달 전부터 저에 대한 임금이 지급되지 않고 있어 병원측에 그 지급을 요구하였으나, 병원측은 레지던트 과정도 교육의 과정이므로 임금을 지급하지 않아도 되며 그 전에 얼마씩 지급한 것도 장학금 내지 생활비조로 병원측에서 호의적으로 지급한 것이었다고 말하고 있습니다. 저는 계속 이를 다투었다가는 장래에 좋지 않은 영향을 줄 것도 같아 어떻게 하여야 할지 모르겠습니다. 좋은 방법이 있는지요?"
-sampleData = similar_precendent.get_similar_precedent(userInput)
+sampleData = similar_precedent.get_similar_precedent(userInput)
 print(sampleData)
 
 print(">>> GPT generating...")
