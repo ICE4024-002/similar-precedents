9293d7f9ee649acf174175592e8f2e742077e199
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-05-27 00:18:38 +0900

    서버 실행 시작 시 판례 텍스트 및 임베드 데이터 로드

---
+++ fastapi/init.py
@@ -0,0 +1,37 @@
+import os
+import torch
+from datasets import load_dataset
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '../.env.local')
+load_dotenv(dotenv_path)
+
+db_url = os.getenv('DB_URL')
+engine = create_engine(db_url)
+connection = engine.connect()
+
+def load_precedents():
+    dataset_id ="joonhok-exo-ai/korean_law_open_data_precedents"
+    dataset = load_dataset(dataset_id)
+    data = dataset['train']
+
+    print('>>> Precedents Text loaded!')
+    return data
+
+def load_precedents_embeddings():
+    query = text(f"""
+    SELECT
+        embedding
+    FROM
+        ko_sbert_not_processed_precedents;
+    """)
+    result = connection.execute(query).fetchall()
+
+    result_embeddings = torch.tensor([
+        [float(num_str) for num_str in row[0][1:-1].split(',')]
+        for row in result
+    ])
+
+    print('>>> Precedents Embeddings loaded!')
+    return result_embeddings    
--- fastapi/main.py
+++ fastapi/main.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
+from contextlib import asynccontextmanager
+import asyncio
 from dto.feedback import models, schemas
 from api import crud
 from api.database import SessionLocal, engine
-from fastapi import FastAPI
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
-
-# Dependency
 def get_db():
     db = SessionLocal()
     try:
@@ -17,5 +15,16 @@ def get_db():
     finally:
         db.close()
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    loop = asyncio.get_event_loop()
+    precedents = await loop.run_in_executor(None, load_precedents)
+    precedents_embeddings = await loop.run_in_executor(None, load_precedents_embeddings)
+
+    app.state.precedents = precedents
+    app.state.precedents_embeddings = precedents_embeddings
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 # 전문가의 평가를 DB에 저장하는 API@app.post("/feedback/", response_model=schemas.FeedBack)def create_feedback(feedback: schemas.FeedbackCreate, db: Session = Depends(get_db)):    return crud.create_feedback(db=db, feedback=feedback)
