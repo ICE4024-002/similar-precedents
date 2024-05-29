16045b0cc17a068564979242f90dd7f48927f4fd
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-05-29 23:17:21 +0900

    이전 질문 데이터가 DB에 없는 경우 예외 처리 코드 수정

--- fastapi/main.py
+++ fastapi/main.py
@@ -82,17 +82,16 @@ def get_gpt_answer(Question: dto.question.schemas.Question, db: Session = Depend
     print("유사 판례 검색 시간:", search_time - embedding_time)
     
     # 유사 질문 검색
-    if app.state.question_embeddings is None:
-        return HTTPException(status_code=404, detail="No previous questions")
-    similar_question_id = get_similar_question(question_vector, app.state.question_embeddings)
-    print(">>> similar_question_id: ", similar_question_id)
+    if app.state.question_embeddings:
+        similar_question_id = get_similar_question(question_vector, app.state.question_embeddings)
+        print(">>> similar_question_id: ", similar_question_id)
     
-    # 유사 질문에 대한 피드백이 없을 경우 None 반환
-    expert_feedback = db.query(dto.feedback.expert.models.ExpertFeedback).filter(dto.feedback.expert.models.ExpertFeedback.qna_id == similar_question_id).first()
-    questioner_feedback = db.query(dto.feedback.questioner.models.QuestionerFeedback).filter(dto.feedback.questioner.models.QuestionerFeedback.qna_id == similar_question_id).first()
-    
-    print(">>> expert_feedback: ", expert_feedback.feedback if expert_feedback else "None")
-    print(">>> questioner_feedback: ", questioner_feedback.feedback if questioner_feedback else "None")
+        # 유사 질문에 대한 피드백이 없을 경우 None 반환
+        expert_feedback = db.query(dto.feedback.expert.models.ExpertFeedback).filter(dto.feedback.expert.models.ExpertFeedback.qna_id == similar_question_id).first()
+        questioner_feedback = db.query(dto.feedback.questioner.models.QuestionerFeedback).filter(dto.feedback.questioner.models.QuestionerFeedback.qna_id == similar_question_id).first()
+        
+        print(">>> expert_feedback: ", expert_feedback.feedback if expert_feedback else "None")
+        print(">>> questioner_feedback: ", questioner_feedback.feedback if questioner_feedback else "None")
     
     # GPT 답변 생성
     answer = get_gpt_answer_by_precedent(Question.question, similar_precedent, similarity)
