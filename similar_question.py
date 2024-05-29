117bba0f42d5739c077da47781f362665b4af340
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-05-29 14:36:31 +0900

    유사 질문 검색 함수 구현

---
+++ similar_question.py
@@ -0,0 +1,11 @@
+import torch
+from similarity import cal_cosine
+
+def get_similar_question(question_vector, result_embeddings):
+    prev_question_embeddings = torch.tensor([row[1] for row in result_embeddings])
+    similarities = cal_cosine(question_vector.unsqueeze(0), prev_question_embeddings)
+
+    max_similarity_idx = similarities.argmax().item()
+    question_id = result_embeddings[max_similarity_idx][0]
+
+    return question_id
